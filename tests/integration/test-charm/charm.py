76f4e550c21c146ec220244afe60acaf25c86d09
Jon Seager <jon@sgrs.uk>
2021-11-05 11:39:57 +0000

    Basic integration tests

---
+++ tests/integration/test-charm/charm.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import logging
+from subprocess import check_output
+from urllib.request import urlopen
+
+from charms.operator_libs_linux.v0 import apt, snap
+from ops.charm import ActionEvent, CharmBase, StartEvent
+from ops.main import main
+from ops.model import ActiveStatus
+
+logger = logging.getLogger(__name__)
+
+
+class TesterCharm(CharmBase):
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.framework.observe(self.on.start, self._on_start)
+        self.framework.observe(self.on.apt_install_action, self._on_apt_install_action)
+        self.framework.observe(
+            self.on.apt_install_external_repo_action, self._on_apt_install_external_repo_action
+        )
+        self.framework.observe(self.on.snap_install_action, self._on_snap_install_action)
+        self.framework.observe(self.on.snap_install_bare_action, self._on_snap_install_bare_action)
+
+    def _on_start(self, _: StartEvent):
+        self.unit.status = ActiveStatus()
+
+    def _on_apt_install_action(self, event: ActionEvent):
+        try:
+            apt.update()
+            apt.add_package("zsh")
+            apt.add_package(["golang-cfssl", "jq"])
+        except apt.PackageNotFoundError:
+            logger.error("A specified package not found in package cache or on system")
+        except apt.PackageError as e:
+            logger.error(f"Could not install package. Reason: {e.message}")
+
+        paths = [self._get_command_path(c) for c in ["zsh", "cfssl", "jq"]]
+        event.set_results({"installed": paths})
+
+    def _on_apt_install_external_repo_action(self, event: ActionEvent):
+        repositories = apt.RepositoryMapping()
+
+        # Get the Hashicorp GPG key
+        key = urlopen("https://apt.releases.hashicorp.com/gpg").read().decode()
+
+        # Add the hashicorp repository if it doesn't already exist
+        if "deb-apt.releases.hashicorp.com-focal" not in repositories:
+            line = "deb [arch=amd64] https://apt.releases.hashicorp.com focal main"
+            # line = "deb https://apt.releases.hashicorp.com focal main"
+            repo = apt.DebianRepository.from_repo_line(line)
+            # Import the repository's key
+            repo.import_key(key)
+            repositories.add(repo)
+
+        apt.update()
+        apt.add_package("terraform")
+        event.set_results({"installed": [self._get_command_path("terraform")]})
+
+    def _on_snap_install_action(self, event: ActionEvent):
+        # Try by initialising the cache first, then using ensure
+        try:
+            cache = snap.SnapCache()
+            juju = cache["juju"]
+            if not juju.present:
+                juju.ensure(snap.SnapState.Latest, classic="True", channel="stable")
+        except snap.SnapError as e:
+            logger.error(f"An exception occurred when installing Juju. Reason: {e.message}")
+
+        event.set_results({"installed": [self._get_command_path("juju")]})
+
+    def _on_snap_install_bare_action(self, event: ActionEvent):
+        snap.add(["charmcraft"], state=snap.SnapState.Latest, classic=True, channel="candidate")
+        event.set_results({"installed": [self._get_command_path("charmcraft")]})
+
+    def _get_command_path(self, command):
+        return check_output(["which", command]).decode().strip()
+
+
+if __name__ == "__main__":
+    main(TesterCharm)
---
+++ tests/integration/test_libraries.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# Copyright 2021 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+
+import logging
+import shutil
+from pathlib import Path
+
+import pytest
+import yaml
+from pytest_operator.plugin import OpsTest
+
+logger = logging.getLogger(__name__)
+
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+
+
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy(ops_test: OpsTest):
+    # Copy the libs into the test-charm
+    shutil.copytree("lib", "tests/integration/test-charm/lib")
+    # Build the charm
+    charm = await ops_test.build_charm("tests/integration/test-charm")
+    # Remove the libs from the test-charm dir again
+    shutil.rmtree("tests/integration/test-charm/lib")
+    await ops_test.model.deploy(charm, application_name="tester")
+    await ops_test.model.wait_for_idle(apps=["tester"], status="active", timeout=1000)
+    assert ops_test.model.applications["tester"].units[0].workload_status == "active"
+
+
+@pytest.mark.abort_on_fail
+async def test_apt_install(ops_test: OpsTest):
+    unit = ops_test.model.applications["tester"].units[0]
+
+    action = await unit.run_action("apt-install")
+    action = await action.wait()
+
+    assert action.results["Code"] == "0"
+    assert action.results["installed"] == "['/usr/bin/zsh', '/usr/bin/cfssl', '/usr/bin/jq']"
+
+
+# @pytest.mark.abort_on_fail
+# async def test_apt_install_external_repo(ops_test: OpsTest):
+#     unit = ops_test.model.applications["tester"].units[0]
+
+#     action = await unit.run_action("apt-install-external-repo")
+#     action = await action.wait()
+
+#     assert action.results["Code"] == "0"
+#     assert action.results["installed"] == "['/usr/bin/terraform']"
+
+
+@pytest.mark.abort_on_fail
+async def test_snap_install(ops_test: OpsTest):
+    unit = ops_test.model.applications["tester"].units[0]
+
+    action = await unit.run_action("snap-install")
+    action = await action.wait()
+
+    assert action.results["Code"] == "0"
+    assert action.results["installed"] == "['/snap/bin/juju']"
+
+
+@pytest.mark.abort_on_fail
+async def test_snap_install_bare(ops_test: OpsTest):
+    unit = ops_test.model.applications["tester"].units[0]
+
+    action = await unit.run_action("snap-install-bare")
+    action = await action.wait()
+
+    assert action.results["Code"] == "0"
+    assert action.results["installed"] == "['/snap/bin/charmcraft']"
