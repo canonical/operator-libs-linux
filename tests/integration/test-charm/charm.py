82a003e68d5fb20200167451bb933c13c3dc80dd
Jon Seager <jon@sgrs.uk>
2021-11-08 11:33:30 +0000

    Added a simple user test

--- tests/integration/test-charm/charm.py
+++ tests/integration/test-charm/charm.py
@@ -6,7 +6,7 @@ import logging
 from subprocess import check_output
 from urllib.request import urlopen
 
-from charms.operator_libs_linux.v0 import apt, snap
+from charms.operator_libs_linux.v0 import apt, passwd, snap
 from ops.charm import ActionEvent, CharmBase, StartEvent
 from ops.main import main
 from ops.model import ActiveStatus
@@ -18,12 +18,19 @@ class TesterCharm(CharmBase):
     def __init__(self, *args):
         super().__init__(*args)
         self.framework.observe(self.on.start, self._on_start)
+        
+        # Debian packaging actions
         self.framework.observe(self.on.apt_install_action, self._on_apt_install_action)
         self.framework.observe(
             self.on.apt_install_external_repo_action, self._on_apt_install_external_repo_action
         )
+
+        # Snap packaging actions
         self.framework.observe(self.on.snap_install_action, self._on_snap_install_action)
         self.framework.observe(self.on.snap_install_bare_action, self._on_snap_install_bare_action)
+
+        # User and group management actions
+        self.framework.observe(self.on.add_user_action, self._on_add_user_action)
 
     def _on_start(self, _: StartEvent):
         self.unit.status = ActiveStatus()
@@ -74,6 +81,16 @@ class TesterCharm(CharmBase):
     def _on_snap_install_bare_action(self, event: ActionEvent):
         snap.add(["charmcraft"], state=snap.SnapState.Latest, classic=True, channel="candidate")
         event.set_results({"installed": [self._get_command_path("charmcraft")]})
+    
+    def _on_add_user_action(self, event: ActionEvent):
+        p = passwd.Passwd()
+        user = passwd.User(
+            name="test-user", 
+            state=passwd.UserState.Present, 
+            shell="/bin/bash",
+            group="admin"
+        )
+        p.add_user(user)
 
     def _get_command_path(self, command):
         return check_output(["which", command]).decode().strip()
