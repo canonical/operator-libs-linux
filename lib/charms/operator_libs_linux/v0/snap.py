ab48725dea20227db1dd895bf07dac7041ba1467
Pete Vander Giessen <petevg@canonical.com>
2021-12-07 09:41:44 -0500

    Bumped LIBPATCH and LIBAPI
    LIBAPI++ because the changes to set and get are not backwards
    compatible.

--- lib/charms/operator_libs_linux/v0/snap.py
+++ lib/charms/operator_libs_linux/v0/snap.py
@@ -77,11 +77,11 @@ logger = logging.getLogger(__name__)
 LIBID = "05394e5893f94f2d90feb7cbe6b633cd"
 
 # Increment this major API version when introducing breaking changes
-LIBAPI = 0
+LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 4
+LIBPATCH = 5
 
 
 def _cache_init(func):
