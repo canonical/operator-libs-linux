efc9d38387149d57e2c19e43580a8f8f02f685bf
Jon Seager <jon@sgrs.uk>
2022-07-28 15:56:37 +0100

    Bump LIBPATCH

--- lib/charms/operator_libs_linux/v1/snap.py
+++ lib/charms/operator_libs_linux/v1/snap.py
@@ -82,7 +82,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 3
+LIBPATCH = 4
 
 
 def _cache_init(func):
