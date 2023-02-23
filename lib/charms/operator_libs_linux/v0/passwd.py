d0ebc9fcdbeb4821087de103d0018e4fb369c629
Ben Hoyt <ben.hoyt@canonical.com>
2023-02-23 16:14:40 +1300

    Bump LIBPATCH for two libraries that had been modified but not bumped (#74)

--- lib/charms/operator_libs_linux/v0/passwd.py
+++ lib/charms/operator_libs_linux/v0/passwd.py
@@ -45,7 +45,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 3
+LIBPATCH = 4
 
 
 def user_exists(user: Union[str, int]) -> Optional[pwd.struct_passwd]:
