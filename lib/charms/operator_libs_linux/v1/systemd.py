2695643d0526749b6c1cc4cb50c4166ae5eb092f
Jon Seager <jon@sgrs.uk>
2023-03-09 11:41:20 +0000

    chore: bump LIBPATCH of libraries that will be affected by #78

--- lib/charms/operator_libs_linux/v1/systemd.py
+++ lib/charms/operator_libs_linux/v1/systemd.py
@@ -60,7 +60,7 @@ LIBAPI = 1
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 1
+LIBPATCH = 2
 
 
 class SystemdError(Exception):
