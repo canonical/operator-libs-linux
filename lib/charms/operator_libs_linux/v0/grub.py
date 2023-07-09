b15fcad1082f2ace8d0f0ad3d01a35d2dd5b75e8
Robert Gildein <robert.gildein@canonical.com>
2023-07-10 00:09:53 +0200

    apply suggestion + add update and remove
    - add update function
    - add remove function

--- lib/charms/operator_libs_linux/v0/grub.py
+++ lib/charms/operator_libs_linux/v0/grub.py
@@ -21,6 +21,7 @@ TODO: add more detailed description
 
 import logging
 import os
+import subprocess
 from pathlib import Path
 from typing import Dict, List, Mapping, Optional, Set
 
@@ -56,9 +57,34 @@ CONFIG_DESCRIPTION = """
 #   info -f grub -n 'Simple configuration'
 """
 
+# TODO: add better description to lib
+
 
 class ValidationError(ValueError):
     """Exception representing value validation error."""
+
+    def __init__(self, key: str, message: str) -> None:
+        self._key = key
+        self._message = message
+        super().__init__(message)
+
+    @property
+    def key(self) -> str:
+        """Get name of key, which cause this error."""
+        return self._key
+
+    @property
+    def message(self) -> str:
+        """Get error message."""
+        return self._message
+
+
+class IsContainerError(Exception):
+    """Exception if local machine is container."""
+
+
+class ApplyError(Exception):
+    """Exception if applying new config failed."""
 
 
 def _parse_config(data: str) -> Dict[str, str]:
@@ -109,6 +135,12 @@ def _save_config(path: Path, config: Dict[str, str], header: str = CONFIG_HEADER
     logger.info("grub config file %s was saved", path)
 
 
+def is_container() -> bool:
+    """Help function to see if local machine is container."""
+    # TODO: implement it
+    return False
+
+
 class GrubConfig(Mapping[str, str]):
     """Grub config object.
 
@@ -121,6 +153,10 @@ class GrubConfig(Mapping[str, str]):
     """
 
     _lazy_data: Optional[Dict[str, str]] = None
+
+    def __init__(self, charm_name: Optional[str] = None) -> None:
+        """Initialize the grub config."""
+        self._charm_name = charm_name
 
     def __contains__(self, key: str) -> bool:
         """Check if key is in config."""
@@ -147,6 +183,25 @@ class GrubConfig(Mapping[str, str]):
 
         return data
 
+    def _apply(self):
+        """Check if an update to /boot/grub/grub.cfg is available."""
+        try:
+            subprocess.check_call(f"grub-mkconfig -o {GRUB_CONFIG}", stderr=subprocess.STDOUT)
+            subprocess.check_call(["/usr/sbin/update-grub"], stderr=subprocess.STDOUT)
+        except subprocess.CalledProcessError as error:
+            logger.exception(error)
+            raise ApplyError("New config check failed.") from error
+
+    def _save_grub_configuration(self) -> None:
+        """Save current gru configuration."""
+        logger.info("saving new grub config to %s", GRUB_CONFIG)
+        applied_configs = {self.path, *self.applied_configs}  # using set to drop duplicity
+        registered_configs = os.linesep.join(
+            FILE_LINE_IN_DESCRIPTION.format(path=path) for path in applied_configs
+        )
+        header = CONFIG_HEADER + CONFIG_DESCRIPTION.format(configs=registered_configs)
+        _save_config(GRUB_CONFIG, self._data, header)
+
     def _set_value(self, key: str, value: str) -> bool:
         """Set new value for key."""
         logger.debug("setting new value %s for key %s", value, key)
@@ -162,31 +217,79 @@ class GrubConfig(Mapping[str, str]):
                 current_value,
                 value,
             )
-            raise ValidationError(f"key {key} already exists and its value is {current_value}")
+            raise ValidationError(
+                f"key {key} already exists and its value is {current_value}", key
+            )
 
         return False
 
-    @property
-    def registered_configs(self) -> List[Path]:
-        """Return list of charms which registered config here."""
-        return [config.absolute() for config in LIB_CONFIG_DIRECTORY.glob("*") if config.is_file()]
-
-    def update(self, charm_name: str, config: Dict[str, str]) -> Set[str]:
-        """Update the Grub configuration."""
+    def _update(self, config: Dict[str, str]) -> Set[str]:
+        """Update data in object."""
+        logger.debug("updating current config")
         changed_keys = set()
         for key, value in config.items():
             changed = self._set_value(key, value)
             if changed:
                 changed_keys.add(key)
 
-        logger.debug("saving copy of charm config to %s", LIB_CONFIG_DIRECTORY)
-        _save_config(LIB_CONFIG_DIRECTORY / charm_name, config)
-        # TODO: check if config is valid `grub-mkconfig -o <path>``
-        logger.info("saving new grub config to %s", GRUB_CONFIG)
-        registered_configs = os.linesep.join(
-            FILE_LINE_IN_DESCRIPTION.format(path=path) for path in self.registered_configs
+        return changed_keys
+
+    @property
+    def charm_name(self) -> str:
+        """Get charm name or use value obtained from JUJU_UNIT_NAME env."""
+        if self._charm_name is None:
+            self._charm_name, *_ = os.getenv("JUJU_UNIT_NAME", "unknown").split("/")
+
+        return self._charm_name
+
+    @property
+    def path(self) -> Path:
+        """Return path for charm config."""
+        return LIB_CONFIG_DIRECTORY / self.charm_name
+
+    @property
+    def applied_configs(self) -> List[Path]:
+        """Return list of charms which registered config in LIB_CONFIG_DIRECTORY."""
+        return sorted(
+            [config.absolute() for config in LIB_CONFIG_DIRECTORY.glob("*") if config.is_file()]
         )
-        header = CONFIG_HEADER + CONFIG_DESCRIPTION.format(configs=registered_configs)
-        _save_config(GRUB_CONFIG, self._data, header)
-        # TODO: update grub, https://git.launchpad.net/charm-sysconfig/tree/src/lib/lib_sysconfig.py
+
+    def remove(self) -> None:
+        """Remove config for charm.
+
+        This function will remove config file for charm and re-create the `95-juju-charm.cfg`
+        grub config file without changes made by this charm.
+        """
+        self.path.unlink()
+        logger.info("charm config file %s was removed", self.path)
+        config = {}
+        for path in self.applied_configs:
+            _config = _load_config(path)
+            logger.debug("load config file %s", path)
+            config.update(_config)
+
+        self._lazy_data = config
+        self._save_grub_configuration()
+        self._apply()
+
+    def update(self, config: Dict[str, str]) -> Set[str]:
+        """Update the Grub configuration."""
+        if is_container():
+            raise IsContainerError("Could not configure grub config on container.")
+
+        snapshot = self._data.copy()
+        try:
+            changed_keys = self._update(config)
+            self._save_grub_configuration()
+            self._apply()
+        except ValidationError:
+            self._lazy_data = snapshot
+            raise
+        except ApplyError:
+            self._lazy_data = snapshot
+            self._save_grub_configuration()  # save snapshot copy of grub config
+            raise
+
+        logger.debug("saving copy of charm config to %s", LIB_CONFIG_DIRECTORY)
+        _save_config(LIB_CONFIG_DIRECTORY / self.charm_name, config)
         return changed_keys
--- tests/unit/test_grub.py
+++ tests/unit/test_grub.py
@@ -1,5 +1,6 @@
 # Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
+import subprocess
 import tempfile
 import unittest
 from pathlib import Path
@@ -28,11 +29,25 @@ EXP_GRUB_CONFIG = {
 }
 
 
+def test_validation_error():
+    """Test validation error and it's properties."""
+    error = grub.ValidationError("test", "test message")
+    with pytest.raises(ValueError):
+        raise error
+
+    assert error.key == "test"
+    assert error.message == "test message"
+
+
 class BaseTestGrubLib(unittest.TestCase):
     def setUp(self) -> None:
         tmp_dir = tempfile.TemporaryDirectory()
         self.tmp_dir = Path(tmp_dir.name)
         self.addCleanup(tmp_dir.cleanup)
+
+        # configured paths
+        grub.LIB_CONFIG_DIRECTORY = self.tmp_dir
+        grub.GRUB_CONFIG = self.tmp_dir / "95-juju-charm.cfg"
 
         # change logger
         mocked_logger = mock.patch.object(grub, "logger")
@@ -149,6 +164,29 @@ class TestGrubConfig(BaseTestGrubLib):
         self.mock_load_config.assert_called_once_with(grub.GRUB_CONFIG)
         self.assertDictEqual(config._data, EXP_GRUB_CONFIG)
 
+    @mock.patch.object(grub, "_save_config")
+    def test_save_grub_config(self, mock_save_config):
+        """Test save grub config."""
+        charm = "charm-c"
+        exp_path = self.tmp_dir / charm
+        exp_configs = [self.tmp_dir / "charm-a", self.tmp_dir / "charm-b"]
+        exp_data = {"GRUB_TIMEOUT": "1"}
+
+        with mock.patch.object(
+            grub.GrubConfig, "path", new=mock.PropertyMock(return_value=exp_path)
+        ) and mock.patch.object(
+            grub.GrubConfig, "applied_configs", new=mock.PropertyMock(return_value=exp_configs)
+        ):
+            config = grub.GrubConfig(charm)
+            config._lazy_data = exp_data
+            config._save_grub_configuration()
+
+        mock_save_config.assert_called_once_with(grub.GRUB_CONFIG, exp_data, mock.ANY)
+        _, _, header = mock_save_config.call_args[0]
+        self.assertIn(str(exp_path), header)
+        for exp_config_path in exp_configs:
+            self.assertIn(str(exp_config_path), header)
+
     def test_set_new_value(self):
         """Test set new value in config."""
         config = grub.GrubConfig()
@@ -169,10 +207,145 @@ class TestGrubConfig(BaseTestGrubLib):
         with pytest.raises(grub.ValidationError):
             config._set_value("GRUB_TIMEOUT", "1")
 
-    def test_registered_configs(self):
-        """Test registered_configs property."""
-        ...
+    def test_update_data(self):
+        """Test update GrubConfig _data."""
+        data = {"GRUB_TIMEOUT": "1", "GRUB_RECORDFAIL_TIMEOUT": "0"}
+        new_data = {"GRUB_NEW_KEY": "test", "GRUB_RECORDFAIL_TIMEOUT": "0"}
 
-    def test_update(self):
+        config = grub.GrubConfig("charm-a")
+        config._lazy_data = data
+        changed_keys = config._update(new_data)
+        self.assertEqual({"GRUB_NEW_KEY"}, changed_keys)
+
+    @mock.patch.object(grub, "os")
+    def test_charm_name(self, mock_os):
+        """Test define charm name or using value from JUJU_UNIT_NAME env."""
+        exp_charm_name = "test-charm"
+        mock_os.getenv.return_value = exp_charm_name
+
+        # define charm name in init
+        config = grub.GrubConfig(exp_charm_name)
+        self.assertEqual(config.charm_name, exp_charm_name)
+        mock_os.assert_not_called()
+
+        # get charm name from env
+        config = grub.GrubConfig()
+        self.assertEqual(config.charm_name, exp_charm_name)
+        mock_os.getenv.assert_called_once_with("JUJU_UNIT_NAME", "unknown")
+
+    def test_path(self):
+        """Test path property."""
+        exp_charm = "charm-a"
+
+        config = grub.GrubConfig(exp_charm)
+        self.assertEqual(config.path, self.tmp_dir / exp_charm)
+
+    def test_applied_configs(self):
+        """Test registered_configs property."""
+        exp_configs = [self.tmp_dir / "charm-a", self.tmp_dir / "charm-b"]
+        [path.touch() for path in exp_configs]
+
+        config = grub.GrubConfig("charm-d")
+        self.assertEqual(config.applied_configs, exp_configs)
+
+    @mock.patch.object(grub, "_load_config")
+    @mock.patch.object(grub.GrubConfig, "_apply")
+    @mock.patch.object(grub.GrubConfig, "_save_grub_configuration")
+    def test_remove(self, mock_save, mock_apply, mock_load_config):
+        """Test removing config for current charm."""
+        exp_charm = "charm-a"
+        exp_configs = {
+            "charm-a": {"GRUB_RECORDFAIL_TIMEOUT": "0"},
+            "charm-b": {"GRUB_TIMEOUT": "0"},
+            "charm-c": {
+                "GRUB_TERMINAL": "console",
+                "GRUB_CMDLINE_LINUX_DEFAULT": '"$GRUB_CMDLINE_LINUX_DEFAULT hugepagesz=1G"',
+            },
+        }
+        mock_load_config.side_effect = reversed(exp_configs.values())
+        [(self.tmp_dir / charm).touch() for charm in exp_configs]  # create files
+
+        config = grub.GrubConfig(exp_charm)
+        config.remove()
+
+        self.assertFalse((self.tmp_dir / exp_charm).exists())
+        mock_load_config.assert_has_calls(
+            [mock.call(self.tmp_dir / charm) for charm in exp_configs if charm != exp_charm]
+        )
+        self.assertDictEqual(
+            config._data,
+            {
+                "GRUB_TIMEOUT": "0",
+                "GRUB_TERMINAL": "console",
+                "GRUB_CMDLINE_LINUX_DEFAULT": '"$GRUB_CMDLINE_LINUX_DEFAULT hugepagesz=1G"',
+            },
+        )
+        mock_save.assert_called_once()
+        mock_apply.assert_called_once()
+
+    @mock.patch.object(grub, "is_container", return_value=True)
+    def test_update_on_container(self, _):
+        """Test update current grub config on container."""
+        config = grub.GrubConfig("charm-a")
+        with self.assertRaises(grub.IsContainerError):
+            config.update({"GRUB_TIMEOUT": "1"})
+
+        self.assertDictEqual(config._data, EXP_GRUB_CONFIG)
+
+    @mock.patch.object(grub, "is_container", return_value=False)
+    @mock.patch.object(grub.GrubConfig, "_save_grub_configuration")
+    def test_update_with_validation_failure(self, mock_save, _):
+        """Test update current grub config with validation error."""
+        config = grub.GrubConfig("charm-a")
+        with self.assertRaises(grub.ValidationError):
+            config.update({"GRUB_TIMEOUT": "1"})
+
+        self.assertDictEqual(config._data, EXP_GRUB_CONFIG)
+        mock_save.assert_not_called()
+
+    @mock.patch.object(grub, "is_container", return_value=False)
+    @mock.patch.object(
+        grub.subprocess,
+        "check_call",
+        side_effect=[subprocess.CalledProcessError(1, ["echo"]), None],
+    )
+    @mock.patch.object(grub.GrubConfig, "_save_grub_configuration")
+    def test_update_with_apply_failure_due_mkconfig(self, mock_save, *_):
+        """Test update current grub config with apply error due mkconfig failure."""
+        config = grub.GrubConfig("charm-a")
+        with self.assertRaises(grub.ApplyError):
+            config.update({"GRUB_TIMEOUT": "0"})
+
+        self.assertDictEqual(config._data, EXP_GRUB_CONFIG)
+        self.assertEqual(len(mock_save.mock_calls), 2)
+
+    @mock.patch.object(grub, "is_container", return_value=False)
+    @mock.patch.object(
+        grub.subprocess,
+        "check_call",
+        side_effect=[None, subprocess.CalledProcessError(1, ["echo"])],
+    )
+    @mock.patch.object(grub.GrubConfig, "_save_grub_configuration")
+    def test_update_with_apply_failure_due_grub_update(self, mock_save, *_):
+        """Test update current grub config with apply error due grub update failure."""
+        config = grub.GrubConfig("charm-a")
+        with self.assertRaises(grub.ApplyError):
+            config.update({"GRUB_TIMEOUT": "0"})
+
+        self.assertDictEqual(config._data, EXP_GRUB_CONFIG)
+        self.assertEqual(len(mock_save.mock_calls), 2)
+
+    @mock.patch.object(grub, "is_container", return_value=False)
+    @mock.patch.object(grub.subprocess, "check_call")
+    @mock.patch.object(grub, "_save_config")
+    @mock.patch.object(grub.GrubConfig, "_save_grub_configuration")
+    def test_update(self, mock_save, mock_save_config, *_):
         """Test update current grub config."""
-        ...
+        exp_charm = "charm-a"
+        exp_config = {"GRUB_TIMEOUT": "0"}
+        config = grub.GrubConfig(exp_charm)
+        config.update(exp_config)
+
+        self.assertDictEqual(config._data, EXP_GRUB_CONFIG)
+        mock_save.assert_called_once()
+        mock_save_config.assert_called_once_with(self.tmp_dir / exp_charm, exp_config)
