dbb2eb339e9a89f558f94035b0f962ec86c99cfc
Raul Zamora <raul.zamora@canonical.com>
2023-08-04 12:22:22 +0200

    increase lib patch

--- lib/charms/operator_libs_linux/v0/sysctl.py
+++ lib/charms/operator_libs_linux/v0/sysctl.py
@@ -84,7 +84,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 3
+LIBPATCH = 4
 
 CHARM_FILENAME_PREFIX = "90-juju-"
 SYSCTL_DIRECTORY = Path("/etc/sysctl.d")
