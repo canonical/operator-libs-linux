60b588649b372f32ef236755c45dbcfc64563828
Jason C. Nucciarone <nuccitheboss@ubuntu.com>
2024-07-19 18:10:03 -0400

    chore: bump libpatch version
    Signed-off-by: Jason C. Nucciarone <nuccitheboss@ubuntu.com>

--- lib/charms/operator_libs_linux/v0/juju_systemd_notices.py
+++ lib/charms/operator_libs_linux/v0/juju_systemd_notices.py
@@ -108,7 +108,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version.
-LIBPATCH = 1
+LIBPATCH = 2
 
 # juju-systemd-notices charm library dependencies.
 # Charm library dependencies are installed when the consuming charm is packed.
