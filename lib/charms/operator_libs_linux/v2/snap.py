9d33a522aad2f6d746fe198f47b38a979a5652dc
James Garner <james.garner@canonical.com>
2025-01-28 15:23:04 +1300

    chore: bump LIBPATCH to release the snap library
    Now that type annotations and linting updates for the snap library are
    complete.

--- lib/charms/operator_libs_linux/v2/snap.py
+++ lib/charms/operator_libs_linux/v2/snap.py
@@ -102,7 +102,7 @@ LIBAPI = 2
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 9
+LIBPATCH = 10
 
 
 # Regex to locate 7-bit C1 ANSI sequences
