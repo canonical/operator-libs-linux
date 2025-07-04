27de3b24171ccd01608fac6942cec27650ad5dec
Dima Tisnek <dimaqq@gmail.com>
2025-07-04 17:04:17 +0900

    bump library patch version

--- lib/charms/operator_libs_linux/v0/apt.py
+++ lib/charms/operator_libs_linux/v0/apt.py
@@ -127,7 +127,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 17
+LIBPATCH = 18
 
 PYDEPS = ["opentelemetry-api"]
 
